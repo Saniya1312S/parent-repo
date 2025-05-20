98a9c6f4c05f6cf99094c001330f9eb82f4073f5
Nagamani-Deeptimaan <nagamani.manne@deeptimaaninfosystem.com>
2025-05-20 15:10:50 +0530

    added log statement

--- app/src/main/java/com/example/uipracticeapplication/MainActivity.java
+++ app/src/main/java/com/example/uipracticeapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.uipracticeapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            Log.d("MainActivity", "Main Activity is launched");
             return insets;
         });
     }
